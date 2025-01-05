4e55518742491063874627bc5b2a4f8ae7dcf89a
Hantong Chen <cxwdyx620@gmail.com>
2025-01-06 00:49:25 +0800

    feat: feat-response

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
 //! miku-http-util
 
 pub mod request;
+#[cfg(feature = "feat-response")]
+pub mod response;
---
+++ src/response.rs
@@ -0,0 +1,38 @@
+//! HTTP response utilities
+
+use bytes::Bytes;
+use http::response::Parts;
+
+#[derive(Debug, Clone)]
+/// Response (Extended)
+pub struct ResponseExt<B = Bytes> {
+    /// HTTP response parts (see [`http::response::Parts`])
+    pub response_parts: Parts,
+
+    /// Body bytes
+    pub body: B,
+}
+
+impl ResponseExt {
+    #[cfg(feature = "feat-response-ext-json")]
+    /// Convert the body to a JSON value
+    ///
+    /// If the body is not valid JSON, the original response is returned as an
+    /// error.
+    pub fn json<T>(self) -> Result<ResponseExt<T>, Self>
+    where
+        T: for<'a> serde::Deserialize<'a>,
+    {
+        match serde_json::from_slice(&self.body) {
+            Ok(body) => Ok(ResponseExt {
+                response_parts: self.response_parts,
+                body,
+            }),
+            Err(e) => {
+                #[cfg(feature = "feat-tracing")]
+                tracing::error!("Failed to parse JSON: {}", e);
+                Err(self)
+            }
+        }
+    }
+}
