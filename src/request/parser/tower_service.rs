8466b9299a65821e17ffde4830032d5cc7afee90
Hantong Chen <cxwdyx620@gmail.com>
2025-02-28 20:07:25 +0800

    feat: required query key

--- src/request/parser/tower_service.rs
+++ src/request/parser/tower_service.rs
@@ -5,53 +5,86 @@ use std::{
     task::{Context, Poll},
 };
 
-use http::{Request, Response};
+use anyhow::Result;
+use http::Request;
 use tower_layer::Layer;
 use tower_service::Service;
 
 use super::OwnedQueries;
 
-#[derive(Debug, Default)]
-/// [`Layer`] for parsing [`OwnedQueries`] from a [`Request`] and insert into
-/// the [`Request`] extensions.
-pub struct QueriesLayer<ReqBody, ResBody> {
-    _req_body: PhantomData<ReqBody>,
-    _res_body: PhantomData<ResBody>,
+#[inline]
+/// Helper function to extract [`OwnedQueries`] from
+/// [`Extensions`](http::Extensions) within given [`Request`].
+pub fn get_queries<ReqBody>(request: &Request<ReqBody>) -> Result<Option<&OwnedQueries>> {
+    match request
+        .extensions()
+        .get::<Result<OwnedQueries, ParseQueriesError>>()
+    {
+        Some(Ok(data)) => Ok(Some(data)),
+        Some(Err(e)) => Err((*e).into()),
+        None => Ok(None),
+    }
 }
 
-impl<ReqBody, ResBody> Clone for QueriesLayer<ReqBody, ResBody> {
+#[derive(Debug, Clone, Copy)]
+#[derive(thiserror::Error)]
+enum ParseQueriesError {
+    #[error("missing query key `{0}`")]
+    MissingKey(&'static str),
+}
+
+#[derive(Debug, Default, Copy)]
+#[repr(transparent)]
+/// [`Layer`] for parsing [`OwnedQueries`] from a [`Request`] and insert into
+/// the [`Request`] extensions.
+pub struct QueriesLayer<ReqBody> {
+    _req_body: PhantomData<ReqBody>,
+    required: &'static [&'static str],
+}
+
+// `ReqBody`, `ResBody` is just type markers, we actually don't care
+// about what actually it is, but the compiler will complain that *`Clone` is
+// needed* if we just `#[derive(Clone)]`
+impl<ReqBody> Clone for QueriesLayer<ReqBody> {
     fn clone(&self) -> Self {
         Self {
             _req_body: PhantomData,
-            _res_body: PhantomData,
+            required: self.required,
         }
     }
 }
 
 #[allow(unsafe_code)]
-unsafe impl<ReqBody, ResBody> Sync for QueriesLayer<ReqBody, ResBody> {}
+// SAFETY: `ReqBody`, `ResBody` is just type markers, we actually don't care
+// about what actually it is, but compiler complains about `the type parameter
+// `B` is not constrained by ***`.
+unsafe impl<ReqBody> Sync for QueriesLayer<ReqBody> {}
 
-impl<ReqBody, ResBody> QueriesLayer<ReqBody, ResBody> {
+impl<ReqBody> QueriesLayer<ReqBody> {
     /// Create a new [`QueriesLayer`].
-    pub const fn new() -> Self {
+    ///
+    /// # Params
+    ///
+    /// - `required`: required query keys
+    pub const fn new(required: &'static [&'static str]) -> Self {
         Self {
             _req_body: PhantomData,
-            _res_body: PhantomData,
+            required,
         }
     }
 }
 
-impl<S, ReqBody, ResBody> Layer<S> for QueriesLayer<ReqBody, ResBody>
+impl<S, ReqBody> Layer<S> for QueriesLayer<ReqBody>
 where
-    S: Service<Request<ReqBody>, Response = Response<ResBody>> + Send + 'static,
+    S: Service<Request<ReqBody>> + Send + 'static,
 {
-    type Service = QueriesServcie<S, ReqBody, ResBody>;
+    type Service = QueriesServcie<S, ReqBody>;
 
     fn layer(&self, inner: S) -> Self::Service {
         QueriesServcie {
             inner,
+            required: self.required,
             _req_body: PhantomData,
-            _res_body: PhantomData,
         }
     }
 }
@@ -59,31 +92,37 @@ where
 #[derive(Debug)]
 /// [`Service`] for parsing [`OwnedQueries`] from a [`Request`] and insert into
 /// the [`Request`] extensions.
-pub struct QueriesServcie<S, ReqBody, ResBody> {
+pub struct QueriesServcie<S, ReqBody> {
     inner: S,
+    required: &'static [&'static str],
     _req_body: PhantomData<ReqBody>,
-    _res_body: PhantomData<ResBody>,
 }
 
-impl<S, ReqBody, ResBody> Clone for QueriesServcie<S, ReqBody, ResBody>
+// `ReqBody`, `ResBody` is just type markers, we actually don't care
+// about what actually it is, but the compiler will complain that *`Clone` is
+// needed* if we just `#[derive(Clone)]`
+impl<S, ReqBody> Clone for QueriesServcie<S, ReqBody>
 where
     S: Clone,
 {
     fn clone(&self) -> Self {
         Self {
             inner: self.inner.clone(),
+            required: self.required,
             _req_body: PhantomData,
-            _res_body: PhantomData,
         }
     }
 }
 
 #[allow(unsafe_code)]
-unsafe impl<S, ReqBody, ResBody> Sync for QueriesServcie<S, ReqBody, ResBody> where S: Sync {}
+// SAFETY: `ReqBody`, `ResBody` is just type markers, we actually don't care
+// about what actually it is, but compiler complains about `the type parameter
+// `B` is not constrained by ***`.
+unsafe impl<S, ReqBody> Sync for QueriesServcie<S, ReqBody> where S: Sync {}
 
-impl<S, ReqBody, ResBody> Service<Request<ReqBody>> for QueriesServcie<S, ReqBody, ResBody>
+impl<S, ReqBody> Service<Request<ReqBody>> for QueriesServcie<S, ReqBody>
 where
-    S: Service<Request<ReqBody>, Response = Response<ResBody>> + Send + 'static,
+    S: Service<Request<ReqBody>> + Send + 'static,
 {
     type Response = S::Response;
     type Error = S::Error;
@@ -98,7 +137,24 @@ where
             #[cfg(feature = "feat-tracing")]
             tracing::trace!("Found queries: {:?}", owned_queries);
 
-            req.extensions_mut().insert(owned_queries);
+            let mut has_error = false;
+            for &key in self.required {
+                if !owned_queries.contains_key(key) {
+                    #[cfg(feature = "feat-tracing")]
+                    tracing::error!(key, "Missing query key");
+
+                    has_error = true;
+                    req.extensions_mut()
+                        .insert(Err::<OwnedQueries, _>(ParseQueriesError::MissingKey(key)));
+
+                    break;
+                }
+            }
+
+            if !has_error {
+                req.extensions_mut()
+                    .insert(Ok::<_, ParseQueriesError>(owned_queries));
+            }
         }
 
         self.inner.call(req)
